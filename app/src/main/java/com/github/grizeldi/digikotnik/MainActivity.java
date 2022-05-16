c66d49b2d0c1dc2f990c27b7eaa258768d45024c
grizeldi <grizeldi@gmail.com>
2022-05-16 12:15:43 +0200

    Basic sensor reading

--- app/src/main/java/com/github/grizeldi/digikotnik/MainActivity.java
+++ app/src/main/java/com/github/grizeldi/digikotnik/MainActivity.java
@@ -5,14 +5,22 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    public static final boolean USE_FALLBACK_SENSORS = false;
     private SensorManager sensorManager;
-    private Sensor rotationVectorSensor;
+    private Sensor accelometerSensor, magneticSensor, dirVectorSensor;
+
+    // Sensor data cache
+    private float[] accelometerCache = new float[3];
+    private float[] magnetCache = new float[3];
+    private float[] directionCache = new float[5];
+
+    private float[] outputCache;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,13 +28,21 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         setContentView(R.layout.activity_main);
 
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        rotationVectorSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+
+        accelometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        magneticSensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        dirVectorSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        sensorManager.registerListener(this, rotationVectorSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        if (USE_FALLBACK_SENSORS) {
+            sensorManager.registerListener(this, accelometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
+            sensorManager.registerListener(this, magneticSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        } else {
+            sensorManager.registerListener(this, dirVectorSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        }
     }
 
     @Override
@@ -37,11 +53,30 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
-        System.out.println("Sensor state: " + Arrays.toString(sensorEvent.values));
+//        System.out.println(sensorEvent.sensor.getName() + " " + Arrays.toString(sensorEvent.values));
+        if (sensorEvent.sensor == accelometerSensor) {
+            accelometerCache = sensorEvent.values;
+        } else if (sensorEvent.sensor == magneticSensor) {
+            magnetCache = sensorEvent.values;
+        } else if (sensorEvent.sensor == dirVectorSensor) {
+            directionCache = sensorEvent.values;
+        }
+        updateValues();
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
 
+    }
+
+    private void updateValues() {
+        final float[] rotationMatrix = new float[9];
+        final float[] orientationAngles = new float[3];
+        if (USE_FALLBACK_SENSORS)
+            SensorManager.getRotationMatrix(rotationMatrix, null, accelometerCache, magnetCache);
+        else
+            SensorManager.getRotationMatrixFromVector(rotationMatrix, directionCache);
+        SensorManager.getOrientation(rotationMatrix, orientationAngles);
+        outputCache = orientationAngles;
     }
 }
