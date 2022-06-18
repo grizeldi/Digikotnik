30f29317ced92089a26d5b1f201578bf5495781a
grizeldi <grizeldi@gmail.com>
2022-06-18 20:14:16 +0200

    Data display to the saved activity

--- app/src/main/java/com/github/grizeldi/digikotnik/SavedActivity.java
+++ app/src/main/java/com/github/grizeldi/digikotnik/SavedActivity.java
@@ -1,7 +1,12 @@
 package com.github.grizeldi.digikotnik;
 
+import android.widget.ListView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.github.grizeldi.digikotnik.data.DatabaseHolder;
+import com.github.grizeldi.digikotnik.data.Measurement;
+
+import java.util.List;
 
 public class SavedActivity extends AppCompatActivity {
 
@@ -9,5 +14,11 @@ public class SavedActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_saved);
+
+        ListView listView = findViewById(R.id.savedList);
+        new Thread(() -> {
+            List<Measurement> entries = DatabaseHolder.database.measurementDao().getLastTenMeasurements();
+            listView.setAdapter(new MeasurementAdapter(SavedActivity.this, entries));
+        }).start();
     }
 }
