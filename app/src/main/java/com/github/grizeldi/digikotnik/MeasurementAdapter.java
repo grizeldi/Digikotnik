a8bfeb8cc63ce3c00a5836310f38fc3bec8cf28c
grizeldi <grizeldi@gmail.com>
2022-06-20 17:09:19 +0200

    Small list UX improvements

--- app/src/main/java/com/github/grizeldi/digikotnik/MeasurementAdapter.java
+++ app/src/main/java/com/github/grizeldi/digikotnik/MeasurementAdapter.java
@@ -4,10 +4,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.TextView;
+import android.widget.*;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import com.github.grizeldi.digikotnik.data.DatabaseHolder;
@@ -44,6 +41,7 @@ public class MeasurementAdapter extends ArrayAdapter<Measurement> {
                 view.post(() -> {
                     model.remove(deletedPosition);
                     notifyDataSetChanged();
+                    Toast.makeText(getContext(), "Measurement deleted.", Toast.LENGTH_SHORT).show();
                 });
             }).start();
         });
